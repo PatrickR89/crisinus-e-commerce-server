537183d365b35862c8c1231889d8da856421eb81
PatrickR89 <patrickr.89sc@gmail.com>
2022-06-22 21:52:51 +0200

    Refactor authors APIs

--- routes/authorRoutes.js
+++ routes/authorRoutes.js
@@ -8,65 +8,67 @@ const router = express.Router();
 let dbP;
 
 const connection = async () => {
-  dbP = await dbAuth;
+    dbP = await dbAuth;
 };
 
 connection();
 
-router.get("/authorlist", async (req, res) => {
-  const [authors] = await dbP.execute("SELECT * FROM authors");
-  res.send(authors);
+router.route("/").get(async (req, res) => {
+    const [authors] = await dbP.execute("SELECT * FROM authors");
+    res.send(authors);
 });
+router
+    .route("/:id")
+    .post(verifyJWT, async (req, res) => {
+        const id = req.body.id;
 
-router.post("/getauthor", verifyJWT, async (req, res) => {
-  const id = req.body.id;
+        const [author] = await dbP.execute(
+            "SELECT * FROM authors WHERE id = ?",
+            [id]
+        );
+        res.send(author);
+    })
+    .put(verifyJWT, async (req, res) => {
+        first_name = req.body.name;
+        last_name = req.body.last_name;
+        images = req.body.images;
+        url = req.body.url;
+        bio = req.body.bio;
+        id = req.body.id;
 
-  const [author] = await dbP.execute("SELECT * FROM authors WHERE id = ?", [
-    id
-  ]);
-  res.send(author);
-});
+        const tempImgs = JSON.stringify(images);
 
-router.put("/editauthor", verifyJWT, async (req, res) => {
-  first_name = req.body.name;
-  last_name = req.body.last_name;
-  images = req.body.images;
-  url = req.body.url;
-  bio = req.body.bio;
-  id = req.body.id;
+        const [editedAuthor] = await dbP.execute(
+            "UPDATE authors SET name = ?, last_name =?, img =?, url =?, bio =? WHERE id =?",
+            [first_name, last_name, tempImgs, url, bio, id]
+        );
 
-  const tempImgs = JSON.stringify(images);
+        res.send(editedAuthor);
+    })
+    .delete(verifyJWT, async (req, res) => {
+        const id = req.body.id;
+        const tempId = JSON.stringify(id);
+        const [author] = await dbP.execute(
+            "SELECT * FROM authors WHERE id = ?",
+            [id]
+        );
+        const [books] = await dbP.execute(
+            `SELECT * FROM books WHERE JSON_CONTAINS(authors, '${tempId}')`
+        );
+        books.forEach(async (book) => {
+            const newAuthors = book.authors.filter((author) => author !== id);
+            const [newAuthorsList] = await dbP.execute(
+                "UPDATE books SET authors = ? WHERE id = ?",
+                [newAuthors, book.id]
+            );
+        });
 
-  const [editedAuthor] = await dbP.execute(
-    "UPDATE authors SET name = ?, last_name =?, img =?, url =?, bio =? WHERE id =?",
-    [first_name, last_name, tempImgs, url, bio, id]
-  );
+        const [delAuthor] = await dbP.execute(
+            "DELETE FROM authors WHERE id = ?",
+            [id]
+        );
 
-  res.send(editedAuthor);
-});
-
-router.delete("/deleteauthor", verifyJWT, async (req, res) => {
-  const id = req.body.id;
-  const tempId = JSON.stringify(id);
-  const [author] = await dbP.execute("SELECT * FROM authors WHERE id = ?", [
-    id
-  ]);
-  const [books] = await dbP.execute(
-    `SELECT * FROM books WHERE JSON_CONTAINS(authors, '${tempId}')`
-  );
-  books.forEach(async (book) => {
-    const newAuthors = book.authors.filter((author) => author !== id);
-    const [newAuthorsList] = await dbP.execute(
-      "UPDATE books SET authors = ? WHERE id = ?",
-      [newAuthors, book.id]
-    );
-  });
-
-  const [delAuthor] = await dbP.execute("DELETE FROM authors WHERE id = ?", [
-    id
-  ]);
-
-  res.send(delAuthor);
-});
+        res.send(delAuthor);
+    });
 
 module.exports = router;
