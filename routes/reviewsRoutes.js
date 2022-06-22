b3e52d78cd13173787e7f14d9ba6724732bdac4e
PatrickR89 <patrickr.89sc@gmail.com>
2022-06-22 22:25:06 +0200

    Refactor ratings/reviews APIs

--- routes/reviewsRoutes.js
+++ routes/reviewsRoutes.js
@@ -10,91 +10,89 @@ const router = express.Router();
 let dbP;
 
 const connection = async () => {
-  dbP = await dbAuth;
+    dbP = await dbAuth;
 };
 
 connection();
 
-router.get("/bookList", async (req, res) => {
-  const [bookList] = await dbP.execute("SELECT id, title FROM books");
+router
+    .route("/")
+    .get(async (req, res) => {
+        const [reviewsList] = await dbP.execute("SELECT * FROM ratings");
+        res.send(reviewsList);
+    })
+    .post(verifyJWT, async (req, res) => {
+        const bookId = req.body.book;
+        const rating_title = req.body.rating_title;
+        const rating = req.body.rating;
+        const reviewer = req.body.reviewer;
+        const review = req.body.review;
 
-  res.send(bookList);
-});
+        const [bookTitle] = await dbP.execute(
+            "SELECT title FROM books WHERE id = ?",
+            [bookId]
+        );
 
-router.post("/addreview", verifyJWT, async (req, res) => {
-  const bookId = req.body.book;
-  const rating_title = req.body.rating_title;
-  const rating = req.body.rating;
-  const reviewer = req.body.reviewer;
-  const review = req.body.review;
+        const [newRating] = await dbP.execute(
+            "INSERT INTO ratings (id, book_id, book_title, rating_title, rating, reviewer, review) VALUES (?, ?, ?, ?, ?, ?, ?)",
+            [
+                uuidv4(),
+                bookId,
+                bookTitle[0].title,
+                rating_title,
+                rating,
+                reviewer,
+                review
+            ]
+        );
+        res.send(newRating);
+    });
 
-  const [bookTitle] = await dbP.execute(
-    "SELECT title FROM books WHERE id = ?",
-    [bookId]
-  );
+router
+    .route("/:id")
+    .post(verifyJWT, async (req, res) => {
+        const id = req.body.id;
 
-  const [newRating] = await dbP.execute(
-    "INSERT INTO ratings (id, book_id, book_title, rating_title, rating, reviewer, review) VALUES (?, ?, ?, ?, ?, ?, ?)",
-    [
-      uuidv4(),
-      bookId,
-      bookTitle[0].title,
-      rating_title,
-      rating,
-      reviewer,
-      review
-    ]
-  );
-  res.send(newRating);
-});
+        const [initialReview] = await dbP.execute(
+            "SELECT * FROM ratings WHERE id =?",
+            [id]
+        );
+        res.send(initialReview[0]);
+    })
+    .put(verifyJWT, async (req, res) => {
+        const rating_id = req.body.id;
+        const bookId = req.body.book;
+        const rating_title = req.body.rating_title;
+        const rating = req.body.rating;
+        const reviewer = req.body.reviewer;
+        const review = req.body.review;
 
-router.get("/reviewsList", async (req, res) => {
-  const [reviewsList] = await dbP.execute("SELECT * FROM ratings");
-  res.send(reviewsList);
-});
+        const [bookTitle] = await dbP.execute(
+            "SELECT title FROM books WHERE id = ?",
+            [bookId]
+        );
 
-router.post("/getInitialReview", verifyJWT, async (req, res) => {
-  const id = req.body.id;
+        const [editedReview] = await dbP.execute(
+            "UPDATE ratings SET book_id = ?, book_title =?, rating_title =?, rating =?, reviewer =?, review =? WHERE id =?",
+            [
+                bookId,
+                bookTitle[0].title,
+                rating_title,
+                rating,
+                reviewer,
+                review,
+                rating_id
+            ]
+        );
+    })
+    .delete(verifyJWT, async (req, res) => {
+        const id = req.body.id;
+        const [delItem] = await dbP.execute(
+            "DELETE FROM ratings WHERE id = ?",
+            [id]
+        );
 
-  const [initialReview] = await dbP.execute(
-    "SELECT * FROM ratings WHERE id =?",
-    [id]
-  );
-  res.send(initialReview[0]);
-});
-
-router.put("/editreview", verifyJWT, async (req, res) => {
-  const rating_id = req.body.id;
-  const bookId = req.body.book;
-  const rating_title = req.body.rating_title;
-  const rating = req.body.rating;
-  const reviewer = req.body.reviewer;
-  const review = req.body.review;
-
-  const [bookTitle] = await dbP.execute(
-    "SELECT title FROM books WHERE id = ?",
-    [bookId]
-  );
-
-  const [editedReview] = await dbP.execute(
-    "UPDATE ratings SET book_id = ?, book_title =?, rating_title =?, rating =?, reviewer =?, review =? WHERE id =?",
-    [
-      bookId,
-      bookTitle[0].title,
-      rating_title,
-      rating,
-      reviewer,
-      review,
-      rating_id
-    ]
-  );
-});
-
-router.delete("/deletereview", verifyJWT, async (req, res) => {
-  const id = req.body.id;
-  const [delItem] = await dbP.execute("DELETE FROM ratings WHERE id = ?", [id]);
-
-  res.send(delItem);
-});
+        res.send(delItem);
+    });
 
 module.exports = router;
